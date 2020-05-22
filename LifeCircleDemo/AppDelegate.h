816452ef80387662f99cb988991ebb12825bf647
aser <2020sshabc@gmmail.com>
2020-05-22 17:55:01 +0800

    Initial Commit

---
+++ LifeCircleDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LifeCircleDemo
+//
+//  Created by Kevin on 2020/5/22.
+//  Copyright © 2020 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LifeCircleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LifeCircleDemo
+//
+//  Created by Kevin on 2020/5/22.
+//  Copyright © 2020 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
